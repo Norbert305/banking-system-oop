0214e0771373757cff6e2c288bbab2e5ab918c9e
Norbert305 <sealsnorbert@yahoo.com>
2023-12-23 00:56:51 -0500

    new commits added

--- oop_project.py
+++ oop_project.py
@@ -1,24 +1,24 @@
-from bank_account import *
+from bank_account import * # import all from the bank_account.py file
+
+# Lets create an instance for our Bank Account.
+Dave = BankAccount(1000, "Dave") # fields are the amount and the name of the account that are stored in the variable Dave.
+Sara = BankAccount(2000, "Sara") # Our banks accounts created with a balance
 
 
-Dave = BankAccount(1000, "Dave")
-Sara = BankAccount(2000, "Sara")
+Dave.getBalance() # This instance outputs the account name and balance from the bank_account.py file.
+Sara.getBalance() # Let's retrieve the account name and balance with getBalance()
+
+Sara.deposit(500) # instance of the current balance + however much you put into the deposit param. 
 
 
-Dave.getBalance()
-Sara.getBalance()
+Dave.withdraw(10000) # instance of the current balance in the account - the amount in the withdraw param.
+Dave.withdraw(10) # If the amount you have doesn't exist in the current bank account their will be an exception handle error.
 
-Sara.deposit(500)
-
-
-Dave.withdraw(10000)
-Dave.withdraw(10)
-
-Dave.transfer(10000, Sara)
-Dave.transfer(100, Sara)
+Dave.transfer(10000, Sara) # Transfers the balance from one user to another
+Dave.transfer(100, Sara) 
 
 
-Jim = InterestRewardsAcct(1000, "Jim")
+Jim = InterestRewardsAcct(1000, "Jim") # Our new interest account of 5%.
 
 Jim.getBalance()
 
@@ -27,7 +27,7 @@ Jim.deposit(100)
 Jim.transfer(100, Dave)
 
 
-Blaze = SavingsAcct(1000, "Blaze")
+Blaze = SavingsAcct(1000, "Blaze") # Our savings account.
 
 Blaze.getBalance()
 
